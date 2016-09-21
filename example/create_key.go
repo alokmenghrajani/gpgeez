abb242bd3665ac5d2235e5ae420de022bde0a08e
Alok Menghrajani <alok@squareup.com>
2016-09-21 11:51:13 -0700

    Integration test

--- example/create_key.go
+++ example/create_key.go
@@ -3,13 +3,13 @@ package main
 import (
   "fmt"
   "time"
-  
+
   "github.com/alokmenghrajani/gpgeez"
 )
 
 func main() {
   config := gpgeez.Config{Expiry: 365 * 24 * time.Hour}
-  key, err := gpgeez.CreateKey("Joe", "test key", "joe@example.com", &config)
+  key, err := gpgeez.CreateKey("JoeJoe", "test key", "joe@example.com", &config)
   if err != nil {
     fmt.Printf("Something went wrong: %v", err)
     return
