55c602682449f33b62cb141b1f862ff95e5b0c06
Alok Menghrajani <alok@squareup.com>
2016-09-28 21:45:09 -0700

    go fmt

--- example/create_key.go
+++ example/create_key.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 
-  "github.com/alokmenghrajani/gpgeez"
+	"github.com/alokmenghrajani/gpgeez"
 )
 
 func main() {
-  config := gpgeez.Config{Expiry: 365 * 24 * time.Hour}
-  key, err := gpgeez.CreateKey("JoeJoe", "test key", "joe@example.com", &config)
-  if err != nil {
-    fmt.Printf("Something went wrong: %v", err)
-    return
-  }
-  output, err := key.Armor()
-  if err != nil {
-    fmt.Printf("Something went wrong: %v", err)
-    return
-  }
-  fmt.Printf("%s\n", output)
+	config := gpgeez.Config{Expiry: 365 * 24 * time.Hour}
+	key, err := gpgeez.CreateKey("JoeJoe", "test key", "joe@example.com", &config)
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		return
+	}
+	output, err := key.Armor()
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		return
+	}
+	fmt.Printf("%s\n", output)
 
-  output, err = key.ArmorPrivate(&config)
-  if err != nil {
-    fmt.Printf("Something went wrong: %v", err)
-    return
-  }
-  fmt.Printf("%s\n", output)
+	output, err = key.ArmorPrivate(&config)
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		return
+	}
+	fmt.Printf("%s\n", output)
 }
